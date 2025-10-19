4f966f84ce082d05be5e56c701d9bf51ccfb4686
Antonia <antoniarata@yahoo.com>
2025-10-19 11:15:56 +0300

    empty spring boot project

---
+++ backend/RaceRoHub/src/main/java/com/racero/hub/RaceRoHubApplication.java
@@ -0,0 +1,13 @@
+package com.racero.hub;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class RaceRoHubApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(RaceRoHubApplication.class, args);
+    }
+
+}
---
+++ backend/RaceRoHub/src/test/java/com/racero/hub/RaceRoHubApplicationTests.java
@@ -0,0 +1,13 @@
+package com.racero.hub;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class RaceRoHubApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
